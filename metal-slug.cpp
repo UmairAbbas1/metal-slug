4f175a9a895e6ced40f0d349f219f39f7535d32e
IzzatKamal <bsai24078@itu.edu.pk>
2025-01-31 10:37:19 +0500

    Update metal-slug.cpp

--- metal-slug.cpp
+++ metal-slug.cpp
@@ -11,10 +11,19 @@ int main()
 	cout << "HEllo world" << endl;
 	cout << "HEllo world" << endl;
 
+
+
+	cout << "HEllo world" << endl;
+	cout << "HEllo world" << endl;
+	cout << "HEllo world" << endl;
+	cout << "HEllo world" << endl;
+	cout << "HEllo world" << endl;
+
+	cout << "HEllo world" << endl;
 	cout << "HEllo world" << endl;
 
 	cout << "baby ";
 	cout << ""; 
-	  
+	cout << "helllo world ";
 	return 0;
 }
