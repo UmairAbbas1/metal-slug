b2d9621779548f1a3a1b8f5766ac042fbcaaee5a
Umair Abbas <bsai24077@itu.edu.pk>
2025-01-31 20:28:56 +0500

    simple as start

--- metal-slug.cpp
+++ metal-slug.cpp
@@ -49,13 +49,125 @@ void Color(int k)
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, k);
 }
+
+
+const int WIDTH = 100;
+const int HEIGHT = 150;
+const int JUMP_HEIGHT = 5;
+const int MAX_BULLETS = 100;
+
+struct Entity {
+    int x, y;
+    bool jumping = false;
+    int jumpCounter = 0;
+    void (*update)(Entity&);
+};
+
+struct Bullet {
+    int x, y;
+    bool active = false;
+};
+
+Bullet bullets[MAX_BULLETS];
+
+
+
+void playerUpdate(Entity& player) 
+{
+    if (_kbhit()) {
+        char key = _getch();
+        switch (key) {
+        case 'a': 
+            if (player.x > 0) 
+                player.x--;
+            break;
+        case 'd':
+            if (player.x < WIDTH - 1) 
+                player.x++;
+            break;
+        case 'w': 
+            if(!player.jumping)
+            { player.jumping = true; player.jumpCounter = JUMP_HEIGHT; }
+            break;
+        case ' ': 
+            // Fire bullet
+            for (int i = 0; i < MAX_BULLETS; i++)
+            {
+                if (!bullets[i].active) {
+                    bullets[i].x = player.x + 2;
+                    bullets[i].y = player.y;
+                    bullets[i].active = true;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+   
+    if (player.jumping) {
+        if (player.jumpCounter > 0) {
+            player.y--;
+            player.jumpCounter--;
+        }
+        else if (player.y < HEIGHT - 1) {
+            player.y++;
+        }
+        else {
+            player.jumping = false;
+        }
+    }
+}
+
+void updateBullets() 
+{
+    for (int i = 0; i < MAX_BULLETS; i++) {
+        if (bullets[i].active) {
+            gotoRowCol(bullets[i].y, bullets[i].x);
+            cout << ' ';
+            bullets[i].x++;
+            if (bullets[i].x >= WIDTH) bullets[i].active = false;
+        }
+    }
+}
+
+void drawBullets() {
+    for (int i = 1; i < MAX_BULLETS; i++) {
+        if (bullets[i].active) {
+            gotoRowCol(bullets[i].y, bullets[i].x);
+            cout << '-';
+        }
+    }
+}
+
+void drawEntity(const Entity& entity) {
+    gotoRowCol(entity.y, entity.x);
+    cout << char(178) << char(178); // Graphical representation of player
+    gotoRowCol(entity.y + 1, entity.x);
+    cout << char(178) << char(178);
+}
+
+void clearEntity(const Entity& entity) {
+    gotoRowCol(entity.y, entity.x);
+    cout << "  ";
+    gotoRowCol(entity.y + 1, entity.x);
+    cout << "  ";
+}
+
 int main()
 {
+    Entity player = { 10, HEIGHT - 2, false, 0, playerUpdate };
+    bool running = true;
 
+    while (running) {
 
-
-
-
-
-	return 0;
+        hideConsoleCursor();
+        clearEntity(player);
+        updateBullets();
+        player.update(player);
+        drawEntity(player);
+        drawBullets();
+        Sleep(100);
+    }
+    return 0;
 }
